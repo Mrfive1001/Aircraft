d4eb282ca3b6f5ab32deef1bee8d6bf3e328ec4b
MrFive <13070156190@163.com>
2018-04-25 16:08:19 +0800

    建立简单模型框架

---
+++ aircraft_obj.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class AircraftEnv:
+    """
+    编写飞行器Markov模型
+    分为几个部分
+    x 表示内部飞行器所有信息
+    state 外部所需要的信息
+    """
+
+    def __init__(self):
+       pass
+
+    def render(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self):
+        pass
