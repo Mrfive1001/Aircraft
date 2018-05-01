af81f5a4488bad3f50609bac6d8a7aa29040377f
MrFive <13070156190@163.com>
2018-05-01 15:06:43 +0800

    增加cav-l气动数据拟合，最大升阻比为2.4

--- aircraft_obj.py
+++ aircraft_obj.py
@@ -41,6 +41,16 @@ class CAV:
         self.beta = 7200
         self.rho0 = 1.225
 
+        # 约束条件
+        self.hf = 20
+        self.vf = 1800
+        self.Q_dot_max_allow = 1200  # Kw/m2
+        self.n_max_allow = 4  # g0
+        self.q_max_allow = 200  # Kpa
+        self.w_Q_dot = 1  # 1
+        self.w_ny = 1  # 10
+        self.w_q = 0.1  # 1
+
     def v2alpha(self, v):
         # 输入速度m/s,输出攻角,这是CAV本身的设计决定的,不需要高边
         v1 = 3100
@@ -65,3 +75,53 @@ class CAV:
         rho = self.rho0 * math.exp(-h / self.beta)
         return rho
 
+    def alphama2clcd(self, alpha, ma):
+        # 攻角马赫数转换为升力系数、阻力系数
+        # cav-l数据，最大升阻比为2.4
+        p00 = -0.2892
+        p10 = 0.07719
+        p01 = 0.03159
+        p20 = -0.0006198
+        p11 = -0.007331
+        p02 = 0.0003381
+        p21 = 8.544e-05
+        p12 = 0.0003569
+        p03 = -0.0001313
+        p22 = -2.406e-06
+        p13 = -5.55e-06
+        p04 = 3.396e-06
+        cl = p00 + p10 * alpha + p01 * ma + p20 * alpha ** 2 + p11 * alpha * ma + p02 * ma ** 2 + \
+             p21 * alpha ** 2 * ma + p12 * alpha * ma ** 2 + p03 * ma ** 3 + p22 * alpha ** 2 * ma ** 2 \
+             + p13 * alpha * ma ** 3 + p04 * ma ** 4
+        p00 = 0.3604
+        p10 = -0.02188
+        p01 = -0.07648
+        p20 = 0.001518
+        p11 = 0.00450
+        p02 = 0.0058
+        p21 = -0.0001462
+        p12 = -0.0001754
+        p03 = -0.0002027
+        p22 = 4.783e-06
+        p13 = 1.025e-06
+        p04 = 2.99e-06
+        cd = p00 + p10 * alpha + p01 * ma + p20 * alpha ** 2 + p11 * alpha * ma + p02 * ma ** 2 + \
+             p21 * alpha ** 2 * ma + p12 * alpha * ma ** 2 + p03 * ma ** 3 + p22 * alpha ** 2 * ma ** 2 \
+             + p13 * alpha * ma ** 3 + p04 * ma ** 4
+        return cl, cd
+
+
+if __name__ == '__main__':
+    cav = CAV()
+    mas = np.array([3.5, 5, 8, 15, 20, 23])
+    alphas = np.array([10, 15, 20])
+    cls = []
+    cds = []
+    for alpha in alphas:
+        temp_cls, temp_cds = [], []
+        for ma in mas:
+            temp_cl, temp_cd = cav.alphama2clcd(alpha, ma)
+            temp_cls.append(temp_cl)
+            temp_cds.append(temp_cd)
+        cls.append(temp_cls.copy())
+        cds.append(temp_cds.copy())
