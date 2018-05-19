1b7eeb2e40b90d152ea1f0f17b03b3b8119327a2
MrFive <13070156190@163.com>
2018-05-19 09:52:37 +0800

    增加matplotlib中文显示，增加特殊字符显示

--- clcd_plot.py
+++ clcd_plot.py
@@ -4,11 +4,17 @@ import matplotlib
 from mpl_toolkits.mplot3d import Axes3D
 from aircraft_obj import CAV
 
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+# 有中文出现的情况，需要u'内容'
+
 # 气动参数测试
 
 # 设置测试范围
-alphas = np.arange(0, 30, 0.5)
-mas = np.arange(0, 20, 0.5)
+alphas = np.arange(10, 20, 0.5)
+mas = np.arange(0, 25, 0.5)
 alphas, mas = np.meshgrid(alphas, mas)
 
 
@@ -29,25 +35,35 @@ def get_clcds(object):
 
     return cls, cds
 
+
 def plot_craft(object):
     cls, cds = get_clcds(object)
     # fig是当前的图
     # ax是当前的轴
     fig = plt.figure()
-    ax = fig.add_subplot(121,projection='3d')
-    ax.invert_xaxis()   # 将x轴方向反转
-    ax.plot_surface(mas, alphas, cds, rstride=1, cstride=1, cmap='rainbow', alpha=0.7)
+    ax = fig.add_subplot(121, projection='3d')
+    ax.set_title(u'升力系数')
+    ax.set_ylabel(r'Mach')
+    ax.set_xlabel(r'$\alpha$/$^\circ$')
+    ax.invert_xaxis()  # 将x轴方向反转
+    ax.plot_surface(alphas, mas, cds, rstride=1, cstride=1, cmap='rainbow')
 
-    ax = fig.add_subplot(122,projection='3d')
+    ax = fig.add_subplot(122, projection='3d')
+    ax.set_title(u'阻力系数')
     ax.invert_xaxis()
-    ax.plot_surface(mas, alphas, cls, rstride=1, cstride=1, cmap='rainbow', alpha=0.7)
+    # ax.set_xlabel(r'$alpha$'+u'(度)')
+    ax.set_ylabel(r'Mach')
+    ax.set_xlabel(r'$\alpha$/$^\circ$')
+    ax.plot_surface(alphas, mas, cls, rstride=1, cstride=1, cmap='rainbow')
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.invert_xaxis()  # 将x轴方向反转
-    cl_cds = cls/cds
-    ax.plot_surface(mas, alphas, cl_cds, rstride=1, cstride=1, cmap='rainbow', alpha=0.7)
-    # TODO 加上标签，整理格式
+    ax.set_ylabel(r'Mach')
+    ax.set_xlabel(r'$\alpha$/$^\circ$')
+    cl_cds = cls / cds
+    ax.plot_surface(alphas, mas, cl_cds, rstride=1, cstride=1, cmap='rainbow')
+
 
 if __name__ == '__main__':
     # 低升阻比
