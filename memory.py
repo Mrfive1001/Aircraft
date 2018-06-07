ab5ff8f345f040516310bd40780eb0b93aa4cc28
MrFive <13070156190@163.com>
2018-06-07 15:52:15 +0800

    增加注释，并整理memory的逻辑

--- memory.py
+++ memory.py
@@ -4,6 +4,7 @@ from aircraft_obj import AircraftEnv
 
 
 def get_memory(groups_num):
+    # 生成数据,生成一次即可
     memory = None  # 存储的记忆数据
     for i in range(groups_num):
         w = i / groups_num
@@ -14,37 +15,41 @@ def get_memory(groups_num):
         else:
             memory = info['store'].copy()
         print('Episode %d,You\'ve stored %d pieces of data.' % (i + 1, len(memory)))
-    np.save('memory.npy', memory)
+    # memory = [w,v,range]
+    np.save('Trajectories\memory_original.npy', memory)
 
 
 def glance_memory(v):
-    memory = np.load('memory.npy')
+    # 得到某个速度下的记忆
+    memory = np.load('Trajectories\memory_original.npy')
     result = (memory[np.fabs(memory[:, 1] - v) < 5]).copy()
     return result
 
 
-def small_memory():
+def small_memory(limit_range):
+    # 将原有记忆进行处理，小于某个range值得记忆进行保存
     try:
-        memory = np.load('memory.npy')
+        memory = np.load('Trajectories\memory_original.npy')
     except Exception:
         get_memory(300)
-        memory = np.load('memory.npy')
-    small_memory = memory[memory[:, -1] <= 10].copy()
-    np.save('small_memory2.npy', small_memory)
+        memory = np.load('Trajectories\memory_original.npy')
+    small_memory = memory[memory[:, -1] <= limit_range].copy()  # 射程限制
+    np.save('Trajectories\memory_%s.npy' % limit_range, small_memory)
 
 
 if __name__ == '__main__':
-    # get_memory(300)
-    vs = [3000, 2500, 2000]
-    result = glance_memory(vs[0])
-    fig = plt.figure()
-    plt.scatter(result[:, 0], result[:, 2])
-    result = glance_memory(vs[1])
-    plt.scatter(result[:, 0], result[:, 2])
-    result = glance_memory(vs[2])
-    plt.scatter(result[:, 0], result[:, 2])
-    plt.legend(['v=%d' % vs[0], 'v=%d' % vs[1], 'v=%d' % vs[2]])
-    plt.xlabel('w')
-    plt.ylabel('range')
-    plt.show()
-    # small_memory()
+    small_memory(300)
+
+    # 画出某个速度下射程和w的关系
+    # vs = [3000, 2500, 2000]
+    # result = glance_memory(vs[0])
+    # fig = plt.figure()
+    # plt.scatter(result[:, 0], result[:, 2])
+    # result = glance_memory(vs[1])
+    # plt.scatter(result[:, 0], result[:, 2])
+    # result = glance_memory(vs[2])
+    # plt.scatter(result[:, 0], result[:, 2])
+    # plt.legend(['v=%d' % vs[0], 'v=%d' % vs[1], 'v=%d' % vs[2]])
+    # plt.xlabel('w')
+    # plt.ylabel('range')
+    # plt.show()
